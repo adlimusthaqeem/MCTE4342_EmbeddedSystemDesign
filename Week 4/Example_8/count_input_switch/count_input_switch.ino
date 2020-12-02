745d4e259e69d7a152bc3c79e82fe0cc4675b0e9
Adli Musthaqeem Bin Abidin <73817240+adlimusthaqeem@users.noreply.github.com>
2020-12-02 15:43:45 +0800

    Add files via upload

---
+++ Week 4/Example_8/count_input_switch/count_input_switch.ino
@@ -0,0 +1,51 @@
+int A = 2;
+int B = 3;
+int C = 4;
+int D = 5;
+int E = 6; // A-E represent the 5 different swirches, Switch A to Switch E
+int currentState = 0;
+int previousState = 0;
+int count[5];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A, INPUT_PULLUP);
+  pinMode(B, INPUT_PULLUP);
+  pinMode(C, INPUT_PULLUP);
+  pinMode(D, INPUT_PULLUP);
+  pinMode(E, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  countUpdate(A);
+  countUpdate(B);
+  countUpdate(C);
+  countUpdate(D);
+  countUpdate(E);
+  Serial.print("A = "), Serial.print(count[0]), Serial.print("   ");
+  Serial.print("B = "), Serial.print(count[1]), Serial.print("   ");
+  Serial.print("C = "), Serial.print(count[2]), Serial.print("   ");
+  Serial.print("D = "), Serial.print(count[3]), Serial.print("   ");
+  Serial.print("E = "), Serial.print(count[4]), Serial.println("   ");
+}
+
+void countUpdate(int pin)
+{
+  if (digitalRead(pin))
+    currentState = 1;
+
+  else
+    currentState = 0;
+
+  if (currentState != previousState)
+  {
+    if (currentState == 1)
+      count[pin - 2] = count[pin - 2] + 1;
+  }
+
+  previousState = currentState;
+  delay(250);
+
+}
---
+++ Week 4/Example_9/BlinkDifferentRates/BlinkDifferentRates.ino
@@ -0,0 +1,46 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
+int led6 = 7;
+int led7 = 8;
+int led8 = 9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  blinkLED(led1, 781, 515);
+  blinkLED(led2, 2014, 1348);
+  blinkLED(led3, 343, 573);
+  blinkLED(led4, 678, 1839);
+  blinkLED(led5, 342, 534);
+  blinkLED(led6, 1478, 326);
+  blinkLED(led7, 1859, 351);
+  blinkLED(led8, 777, 888);
+}
+
+
+void blinkLED(int pin, unsigned long durOn, unsigned long durOff)
+{
+  digitalWrite(pin, HIGH);
+  delay(durOn);
+  digitalWrite(pin, LOW);
+  delay(durOff);
+}
+
