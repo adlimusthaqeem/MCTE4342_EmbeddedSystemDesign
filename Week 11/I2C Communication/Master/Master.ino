a630ecdc5d5400de543725a2d0333fa5125c711c
Adli Musthaqeem Bin Abidin <73817240+adlimusthaqeem@users.noreply.github.com>
2021-01-04 11:32:16 +0800

    Add files via upload
    This is the sample code along with circuit setup diagram, and output video with explanation. The communication protocol in this file is Serial UART communication and I2C Communication

---
+++ Week 11/I2C Communication/Master/Master.ino
@@ -0,0 +1,27 @@
+//i2c Master Code(UNO)
+#include <Wire.h>
+
+void setup()
+{
+  pinMode(8, INPUT_PULLUP);
+  Serial.begin(9600);
+
+  Wire.begin();
+}
+
+void loop()
+{
+  if (digitalRead(8) == HIGH)
+  {
+    Wire.beginTransmission(5);
+    Wire.write('1');
+    Wire.endTransmission();
+  }
+  else
+  {
+    Wire.beginTransmission(5);
+    Wire.write('0');
+    Wire.endTransmission();
+  }
+}
+
