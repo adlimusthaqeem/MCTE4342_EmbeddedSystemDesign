c589483f07c474bb3590af754c8ff55a2e108017
Adli Musthaqeem Bin Abidin <73817240+adlimusthaqeem@users.noreply.github.com>
2021-01-13 13:00:05 +0800

    Add files via upload

---
+++ Week 12/SPI Communication/masterMPU/masterMPU.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+short yawAngle;
+short setYawAngle;
+
+void mpuRead ()
+{
+  digitalWrite(SS, LOW);
+  transferAndWait (0);
+  yawAngle = transferAndWait (0);
+  //yawAngle += transferAndWait (0);
+  digitalWrite(SS, HIGH);
+}
+
+byte transferAndWait (const byte what)
+{
+  int a = SPI.transfer (what);
+  delayMicroseconds (20);
+  return a;
+} // end of transferAndWait
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (115200);
+  digitalWrite(SS, HIGH);  // ensure SS stays high for now
+  SPI.begin ();
+
+  // Slow down the master a bit
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  mpuRead();
+  Serial.println(yawAngle);
+}
---
+++ Week 12/SPI Communication/slaveMpu/slaveMpu.ino
@@ -0,0 +1,153 @@
+#include <SPI.h>
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+int reading;
+int mapValue;
+int i, j;
+MPU6050 mpu;
+#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_QUATERNION
+//#define INTERRUPT_PIN 19  // use pin 2 on Arduino Uno & most boards
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+float angle;
+byte ref;
+short yawAngle;
+short setYawAngle;
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+bool flag;
+bool finish1, finish2;
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void mpuSetup () 
+{
+      // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    Serial.begin(115200);
+    mpu.initialize();
+//    pinMode(INTERRUPT_PIN, INPUT);
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(220); //220
+    mpu.setYGyroOffset(76); //76
+    mpu.setZGyroOffset(-85); // -85
+    mpu.setZAccelOffset(1788); //1788 
+
+    if (devStatus == 0) {
+        mpu.setDMPEnabled(true);
+//        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+}
+
+short mpuRead ()
+{
+//    if (!dmpReady) return;
+//
+//    while (!mpuInterrupt && fifoCount < packetSize) {
+//    }
+//
+//    // reset interrupt flag and get INT_STATUS byte
+//    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    if (mpuIntStatus & 0x02) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+            
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        yawAngle = short(ypr[0] * 180/M_PI);
+        yawAngle = yawAngle < 0 ? 360 + yawAngle : yawAngle;
+    }
+    return yawAngle;
+}
+
+ISR (SPI_STC_vect)
+{
+  SPDR = mapValue;
+//  if (reading > 253 or finish1)
+//  {
+//    if (!flag)
+//      SPDR = 253, finish1 = true, flag = true;
+//    else
+//      SPDR = reading - 253, finish1 = false, flag = false;
+//  }
+//
+////  else if (reading<256)
+////  SPDR = reading;
+//  else if (reading < 253 or finish2)
+//  {
+//    if (!flag)
+//      SPDR = reading, finish2 = true, flag = true;
+//    else
+//      SPDR = 0, finish2 = false, flag = false;
+//  }
+}  // end of interrupt service routine (ISR) SPI_STC_vect
+
+void setup()
+{
+
+  Serial.begin(115200);
+  pinMode(MISO, OUTPUT);
+
+  // turn on SPI in slave mode
+  SPCR |= _BV(SPE);
+
+  // turn on interrupts
+  SPCR |= _BV(SPIE);
+  
+  mpuSetup();
+}
+
+void loop()
+{
+  reading = mpuRead();
+  mapValue = map(reading, 0, 360, 0, 255);
+  Serial.println(mapValue);
+
+  
+}
+
+
